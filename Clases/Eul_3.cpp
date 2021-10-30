e1a178d531d671f47f8474689de9a2872ddec0c3
Juan Manuel (windows) <jgarciamej@unal.edu.co>
2021-10-29 22:17:13 -0500

    Mover y optimizar en clase

--- Clases/Eul_3.cpp
+++ Clases/Eul_3.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <cassert>
 
 bool is_prime(long long num);
 long long largprimdiv(long long nn);
@@ -6,29 +8,29 @@ long long largprimdiv(long long nn);
 int main(void) {
   const long long num = 600851475147;
   std::cout << "\nDivisor: " << largprimdiv(num) << "\n \n";
+  assert(largprimdiv(13) == 13);
 
   return 0;
 }
 
 long long largprimdiv(long long nn) {
-  bool chk = false;
-  int div_sf = -1;
+  long long res = nn;
 
-  for(long long i = 2; i < nn; i++) {
+  for(long long i = 2; i <= nn/2; i++) {
     if(nn % i == 0) {
+      long long lard = nn/i;
       if((is_prime(nn/i))) {
-        div_sf = i;
-        chk = true;
+        res = lard;
         break;
       }
     }
   }
-  return nn/div_sf;
+  return res;
 }
 
 bool is_prime(long long num){
   bool chk = true;
-  for(int i = 2; i < num; i++){
+  for(int i = 2; i < sqrt(num)+1; i++){
     if(num % i == 0){
       chk = false;
       break;
