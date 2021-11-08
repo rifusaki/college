03fd45ad0784c80d221da5b9099c1281d2811707
Juan Manuel (windows) <jgarciamej@unal.edu.co>
2021-11-08 12:51:02 -0500

    Agregado método de Simpson

--- Clases/Integration.cpp
+++ Clases/Integration.cpp
@@ -5,6 +5,7 @@ using fptr = double(double);
 
 double fnc (double x);
 double trape (double a, double b, double n, fptr f);
+double simp (double a, double b, double n, fptr f);
 
 int main(int argc, char **argv) {
   std::cout.precision(15); std::cout.setf(std::ios::scientific); // Precisión de imprimir
@@ -18,7 +19,7 @@ int main(int argc, char **argv) {
   std::cout << xsol << "\n";
 
   for (double h = 1.0e-1; h >= 1.0e-8; h /= 10.0) { // Promedio con h particion - prueba
-    double tsol = trape(xmin, xmax, h, fnc);
+    double tsol = simp(xmin, xmax, h, fnc);
     std::cout << h << "\t" << tsol << "\t" << std::fabs(1-std::fabs((tsol/xsol))) << "\n";
   }
   return 0;
@@ -37,5 +38,25 @@ double trape (double a, double b, double n, fptr f) {
     res += f(xi);
   }
   res = n*(res+((f(a) + f(b))/2));
+  return res;
+}
+
+double simp (double a, double b, double n, fptr f) {
+  double res = 0.0;
+  const int h = std::floor((b-a)/n);
+  bool chk = false;
+
+  for (int i = 1; i < h; i++) {
+    double xi = a + i*n;
+    if (chk == true) {
+      res += 2*f(xi);
+      chk = false;
+    }
+    else if (chk == false) {
+      res += 4*f(xi);
+      chk = true;
+    }
+  }
+  res = n/3*(res+f(a)+f(b));
   return res;
 }
