c4d1f69b7ba97fe437021a6fcbbca52cf7e5c686
Juan Manuel (windows) <jgarciamej@unal.edu.co>
2021-12-07 08:27:41 -0500

    Corregio pofavo

--- Tareas/EnergiaMec/md-main.cpp
+++ Tareas/EnergiaMec/md-main.cpp
@@ -1,11 +1,11 @@
 #include "md-simul.h" // this has all functions declarations
+#include <cmath>
 
 int main(int argc, char **argv)
 {
   Particle ball;
   double DT = std::atof(argv[1]);
   int NSTEPS = int(TF/DT);
-  std::cout << NSTEPS << "*\t" << DT << "=\t" << TF << "\n\n";
 
   // start system
   initial_conditions(ball);
@@ -18,11 +18,11 @@ int main(int argc, char **argv)
   for(int istep = 0; istep < NSTEPS; ++istep) {
     time_integration(ball, DT);
     compute_force(ball);
-    double E = ball.U+ball.K+ball.R;
-    if(istep == 0){E0 = E;}
+    double E = ball.U+ball.K+ball.R; // E Mecánica
+    if(istep == 0){E0 = E;}          // Em inicial
     print(ball, istep*DT);
     std::cout << E << "\t" //11
-              << abs(E/E0)/E0 << "\n"; // 12
+              << (fabs(E-E0))/E0 << "\n"; // 12
   }
 
   return 0;
