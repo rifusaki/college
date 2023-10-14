d7bfeb93156dc49617fd0499d83f7a50ce7c908b
rifusaki <46846373+rifusaki@users.noreply.github.com>
2023-10-14 12:32:33 -0500

    second graph of electric equivalent

--- thermodynamics/exp3-4/equiv-electrico.R
+++ thermodynamics/exp3-4/equiv-electrico.R
@@ -43,7 +43,7 @@ data_2$Q <- with(data_2,
 model_1 <- lm(W ~ Q, data = data_1)
 cat("  [1] El equivalente es", unlist(model_1$coefficients[[2]]), "J/cal\n")
 
-model_2 <- lm(W ~ Q, data = data_2)
+model_2 <- lm(W ~ Q, data = data_2, subset = (W > 0))
 cat("  [2] El equivalente es", unlist(model_2$coefficients[[2]]), "J/cal\n")
 
 #   Gráficas
@@ -58,6 +58,33 @@ ggplot(data = data_1) +
                      labels = c("Datos experimentales", "Ajuste"),
                      name = "Leyenda") +
   ggtitle("Serie 1: Trabajo y calor") +
+  scale_x_continuous(name = "Q") +
+  scale_y_continuous(name = "W") +
+  theme_light() +
+  theme(plot.title = element_text(size = 30, face = "bold",
+                                  hjust = 0.5, family = "AvantGarde"),
+        axis.text = element_text(size = 25, family = "AvantGarde"),
+        axis.title = element_text(size = 30, family = "AvantGarde"),
+        axis.line.x.bottom = element_line(color = "black", size = 1.1),
+        axis.line.y.left = element_line(color = "black", size = 1.1),
+        legend.title = element_text(size = 22, family = "AvantGarde"),
+        legend.text = element_text(size = 20, family = "AvantGarde"),
+        legend.position = c(0.87, 0.1),
+        legend.background = element_rect(fill = "white", color = "grey"))
+dev.off()
+
+png(file = "thermodynamics\\exp3-4\\model_2.png",
+    width = 1720, height = 1080, res = 100)
+data_2_sub <- subset(data_2, Q > 0)
+ggplot(data = data_2_sub) +
+  geom_line(data = data.frame(W_pred = predict(model_2, data_2_sub),
+                              Q = data_2_sub$Q), aes(x = Q, y = W_pred,
+                                                 color = "Ajuste"), size = 3) +
+  geom_point(aes(x = Q, y = W, color = "Datos experimentales"), size = 7) +
+  scale_color_manual(values = c("#ae3ac5", "#f1a10b"),
+                     labels = c("Datos experimentales", "Ajuste"),
+                     name = "Leyenda") +
+  ggtitle("Serie 2: Trabajo y calor") +
   scale_x_continuous(name = "Q") +
   scale_y_continuous(name = "W") +
   theme_light() +
